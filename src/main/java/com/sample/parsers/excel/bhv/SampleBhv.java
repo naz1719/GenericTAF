0cfa771c54304336cc867b477c2d55164ddf0abf
Nazarii Khimin <khimin1719@gmail.com>
2018-06-16 18:36:43 +0300

    set like easier version of excel parser

--- src/main/java/com/sample/parsers/excel/bhv/SampleBhv.java
+++ src/main/java/com/sample/parsers/excel/bhv/SampleBhv.java
@@ -8,6 +8,7 @@ import org.apache.poi.ss.usermodel.Sheet;
 
 import static com.sample.parsers.excel.utils.CellStyleUtils.getCellSHeaderStyle;
 import static com.sample.constants.CommonConsts.*;
+import static com.sample.parsers.excel.utils.CellStyleUtils.getStandardCellStyle;
 
 public class SampleBhv extends BaseBhv {
     private BaseExcel baseExcel = new BaseExcel(FILE_PATH).openFile();
@@ -31,30 +32,22 @@ public class SampleBhv extends BaseBhv {
         baseExcel.saveChangesToFile();
     }
 
-//
+
 //    public void writeBodyOfCell(List<Blog> list) {
-//
+//        CellStyle cellStyle = getStandardCellStyle(baseExcel);
 //        for (int i = 0; i < list.size(); i++) {
 //            Row row1 = createCustomRow();
-//
-//            Cell cell1 = row1.createCell(0);
-//            cell1.setCellValue(list.get(i).getTitle());
-//            cell1.setCellStyle(getStandardCellStyle());
-//
-//            Cell cell2 = row1.createCell(1);
-//            cell2.setCellValue(list.get(i).getURL());
-//            cell2.setCellStyle(getStandardCellStyle());
+//            writeCell(row1, 0, list.get(i).getTitle(), cellStyle);
+//            writeCell(row1, 1, list.get(i).getURL(), cellStyle);
 //        }
-//
 //        baseExcel.saveChangesToFile();
 //    }
-
-
 
 
     public Row createCustomRow() {
         return sheet.createRow(BEGIN_ROW_CREATED_SHEET++);
     }
+
     public Row getRow() {
         return sheet.createRow(BEGIN_ROW_CREATED_SHEET++);
     }
